d6eada818e81e70f9cbe2bf1e1c6bd4a06006162
Evgen1994-McDgit config --global user.email mrmarwell@gmail.comgit config --listgit config --global user.name Evgen1994-McD <mrmarwell@gmail.com>
2025-01-18 22:47:06 +0500

    Спринт 8 Домашка 2
    Ребаз чтобы ревьюер увидел домашку 2 ( в истории коммитов ее не было)

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val searchClicker = findViewById<Button>(R.id.search_day)
+        searchClicker.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на поиск!", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+
+
+            val mediaClicker = findViewById<Button>(R.id.media_day)
+        mediaClicker.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Нажали на медиа!", Toast.LENGTH_SHORT).show()
+
+        }
+
+        val settingsClicker = findViewById<Button>(R.id.settings_day)
+        settingsClicker.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Нажали на настройки!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
