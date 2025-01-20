01e1e7382a085e2abb0b7962764774b64b652f44
Evgen1994-McDgit config --global user.email mrmarwell@gmail.comgit config --listgit config --global user.name Evgen1994-McD <mrmarwell@gmail.com>
2025-01-20 22:31:24 +0500

    HW_3 Спринт 8
    Сделал код ко всем заданиям, нужно проверить разметку и отступы.

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,30 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
+        }
+        val searchClicker = findViewById<Button>(R.id.search_day)
+        searchClicker.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val context = v?.context ?: return // Получаем контекст из представления
+                val displayIntentSrc = Intent(context, SearchActivity::class.java)
+                startActivity(displayIntentSrc)
+            }
+        })
+
+
+        val mediaClicker = findViewById<Button>(R.id.media_day)
+        mediaClicker.setOnClickListener{
+            val displayIntentMedia = Intent(this,MediaActivity::class.java)
+            startActivity(displayIntentMedia)
+
+        }
+        val settingsClicker = findViewById<Button>(R.id.settings_day)
+        settingsClicker.setOnClickListener{
+           val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
         }
     }
+
 }
