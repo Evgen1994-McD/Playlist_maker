6268e45f869d59fbeb2a0fa8679423a91d5ed072
Evgen1994-McDgit config --global user.email mrmarwell@gmail.comgit config --listgit config --global user.name Evgen1994-McD <mrmarwell@gmail.com>
2025-02-08 23:24:00 +0500

    Сделал кнопку "Поделиться приложением"

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
@@ -22,11 +23,24 @@ class SettingsActivity : AppCompatActivity() {
         }
         val backClicker = findViewById<androidx.appcompat.widget.Toolbar>(R.id.settings_toolbar_day)
         backClicker.setNavigationOnClickListener{
-
-
             finish()
         }
+        val shareTheAppClicker = findViewById<com.google.android.material.textview.MaterialTextView>(R.id.shareApp)
+        shareTheAppClicker.setOnClickListener{
+            shareApp(this@SettingsActivity)
+        }
 
+    }
 
+    fun shareApp(context: Context) {  // Метод - интент для отправки сообщений
+        val sendIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/android-developer/?from=catalog") // Ссылка на курс андроид разработки
+            type = "text/plain"
+        }
+
+        context.startActivity(
+            Intent.createChooser(sendIntent, "Поделитесь нашим приложением")
+        )
     }
 }
