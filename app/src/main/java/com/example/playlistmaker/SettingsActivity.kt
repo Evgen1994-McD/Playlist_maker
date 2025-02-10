1b3bdbac0d8201cfbfec5da65f158b91712f055b
Evgen1994-McDgit config --global user.email mrmarwell@gmail.comgit config --listgit config --global user.name Evgen1994-McD <mrmarwell@gmail.com>
2025-02-10 23:48:16 +0500

    Написать в поддержку

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -2,8 +2,10 @@ package com.example.playlistmaker
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.Toast
 import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -22,25 +24,56 @@ class SettingsActivity : AppCompatActivity() {
 
         }
         val backClicker = findViewById<androidx.appcompat.widget.Toolbar>(R.id.settings_toolbar_day)
-        backClicker.setNavigationOnClickListener{
+        backClicker.setNavigationOnClickListener {
             finish()
         }
-        val shareTheAppClicker = findViewById<com.google.android.material.textview.MaterialTextView>(R.id.shareApp)
-        shareTheAppClicker.setOnClickListener{
+        val shareTheAppClicker =
+            findViewById<com.google.android.material.textview.MaterialTextView>(R.id.shareApp)
+        shareTheAppClicker.setOnClickListener {
             shareApp(this@SettingsActivity)
         }
 
+        val mesToSuppClicker =
+            findViewById<com.google.android.material.textview.MaterialTextView>(R.id.sMesToSuport)
+        mesToSuppClicker.setOnClickListener {
+          sendSuppEmail()  //Здесь будет вызван метод
+        }
     }
 
-    fun shareApp(context: Context) {  // Метод - интент для отправки сообщений
-        val sendIntent = Intent().apply {
-            action = Intent.ACTION_SEND
-            putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/android-developer/?from=catalog") // Ссылка на курс андроид разработки
-            type = "text/plain"
+        fun shareApp(context: Context) {  // Метод - интент для отправки сообщений
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    "https://practicum.yandex.ru/android-developer/?from=catalog"
+                ) // Ссылка на курс андроид разработки
+                type = "text/plain"
+            }
+
+            context.startActivity(
+                Intent.createChooser(sendIntent, "Поделитесь нашим приложением")
+            )
         }
 
-        context.startActivity(
-            Intent.createChooser(sendIntent, "Поделитесь нашим приложением")
-        )
+
+    private fun sendSuppEmail(){
+        val myEmail = "mrmarwell@gmail.com"
+        val subject = "Сообщение разработчиками и разработчицам приложением Playlist Maker"
+        val body = "Спасибо разработчикам и разработчицам за крутое приложение!"
+
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            type = "message/rfc822"
+
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(myEmail))
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+            putExtra(Intent.EXTRA_TEXT, body)
+        }
+        if (intent.resolveActivity(packageManager)!= null){
+            startActivity(intent)
+        }
+        else{
+            Toast.makeText(this, "Приложения для отправки отстуствуют!!!", Toast.LENGTH_SHORT).show()
+        }
     }
-}
+
+    }
