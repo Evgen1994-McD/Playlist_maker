3a4d37032edae1beebe7bc4856eaa99cfd9325b3
Evgen1994-McDgit config --global user.email mrmarwell@gmail.comgit config --listgit config --global user.name Evgen1994-McD <mrmarwell@gmail.com>
2025-02-13 21:39:16 +0500

    Сверстал поиск

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -14,13 +14,18 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.search_activity_layout)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.searchLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val backClicker =
+            findViewById<androidx.appcompat.widget.Toolbar>(R.id.search_toolbar) // Назад в MainActivity
+        backClicker.setNavigationOnClickListener {
+            finish()
 
 
-}
+        }
 
+    }
 }
