1a91cfac65719b81b2ef7460eebf5514085edeeb
Evgen1994-McD <mrmarwell@gmail.com>
2025-03-13 17:45:13 +0500

    Исправление комментариев

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -9,12 +9,13 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -30,8 +31,10 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.IOException
 
+
 class SearchActivity : AppCompatActivity() {
-    private lateinit var clearEditText: EditText
+    private lateinit var clearEditText: AppCompatEditText
+
     private lateinit var txtForSearch: String
     private var textFromInput: String = null.toString()
     private val keyForWatcher: String =
@@ -54,10 +57,12 @@ class SearchActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+       clearEditText =  // инициализирую эдиттекст
+            findViewById<AppCompatEditText>(R.id.search_stroke)
+
         phForNothingToShow =
             findViewById<ImageView>(R.id.ph_ntsh_120)
-        clearEditText =
-            findViewById<androidx.appcompat.widget.AppCompatEditText>(R.id.search_stroke)
+
 
         msgTopTxt =
             findViewById<TextView>(R.id.msg_noint_top_txt)
@@ -69,7 +74,7 @@ class SearchActivity : AppCompatActivity() {
             findViewById<TextView>(R.id.button_nointernet)
 
         recyclerView =
-            findViewById<androidx.recyclerview.widget.RecyclerView>(R.id.track_list)
+            findViewById<RecyclerView>(R.id.track_list)
         val interceptor =
             HttpLoggingInterceptor()  // Вылетает при запросах, пробую интерсепотором понять почему
         interceptor.level = HttpLoggingInterceptor.Level.BODY
@@ -84,11 +89,11 @@ class SearchActivity : AppCompatActivity() {
             .addConverterFactory(GsonConverterFactory.create()).build()
         val iTunesApi = retroFit.create(ITunesApi::class.java)
         buttonNoInternet.setOnClickListener { // Кнопка поиска при отсутствии интернета
-            phForNothingToShow.visibility = View.GONE
-            recyclerView.visibility = View.GONE
-            msgTopTxt.visibility = View.GONE
-            msgBotTxt.visibility = View.GONE
-            buttonNoInternet.visibility = View.GONE
+            phForNothingToShow.makeGone()
+            recyclerView.makeGone()
+            msgTopTxt.makeGone()
+            msgBotTxt.makeGone()
+            buttonNoInternet.makeGone()
             txtForSearch = clearEditText.text.toString() // текст для поиска
             searchSongs(
                 txtForSearch,
@@ -98,11 +103,11 @@ class SearchActivity : AppCompatActivity() {
         }
         clearEditText.setOnEditorActionListener { _, actionId, _ ->        // слушатель done-enter
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-                phForNothingToShow.visibility = View.GONE
-                recyclerView.visibility = View.GONE
-                msgTopTxt.visibility = View.GONE
-                msgBotTxt.visibility = View.GONE
-                buttonNoInternet.visibility = View.GONE
+                phForNothingToShow.makeGone()
+                recyclerView.makeGone()
+                msgTopTxt.makeGone()
+                msgBotTxt.makeGone()
+                buttonNoInternet.makeGone()
                 txtForSearch = clearEditText.text.toString() // текст для поиска
                 searchSongs(txtForSearch, iTunesApi) // передаю параметры для поиска в метод
                 true
@@ -113,7 +118,7 @@ class SearchActivity : AppCompatActivity() {
 
 
         val backClicker =
-            findViewById<androidx.appcompat.widget.Toolbar>(R.id.search_toolbar) // Назад в MainActivity
+            findViewById<Toolbar>(R.id.search_toolbar) // Назад в MainActivity
         backClicker.setNavigationOnClickListener {
             finish()
         }
@@ -165,7 +170,8 @@ class SearchActivity : AppCompatActivity() {
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-
+        var clearEditText: EditText =  // инициализирую эдиттекст
+            findViewById<androidx.appcompat.widget.AppCompatEditText>(R.id.search_stroke)
         // Извлечение данных из Bundle
         val savedText = savedInstanceState.getString(keyForWatcher)
         if (savedText != null) {
@@ -174,34 +180,34 @@ class SearchActivity : AppCompatActivity() {
     }
 
 
-    private fun logicClearIc(s: CharSequence?) {
-        val clearEditText =  // инициализирую эдиттекст
-            findViewById<androidx.appcompat.widget.AppCompatEditText>(R.id.search_stroke)
-        if (!s.isNullOrBlank()) {  // Перенести в функцию
-            clearEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(
-                ContextCompat.getDrawable(this@SearchActivity, R.drawable.ic_hintsearch_16),
-                null,
-                ContextCompat.getDrawable(this@SearchActivity, R.drawable.ic_clear_16),
-                null
-            )
-            textFromInput = s.toString()
+        private fun logicClearIc(s: CharSequence?) {
+            var clearEditText=  // инициализирую эдиттекст
+                findViewById<AppCompatEditText>(R.id.search_stroke)
+            if (!s.isNullOrBlank()) {  // Перенести в функцию
+                clearEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                    ContextCompat.getDrawable(this@SearchActivity, R.drawable.ic_hintsearch_16),
+                    null,
+                    ContextCompat.getDrawable(this@SearchActivity, R.drawable.ic_clear_16),
+                    null
+                )
+                textFromInput = s.toString()
 
-        } else {
-            clearEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(
-                ContextCompat.getDrawable(this@SearchActivity, R.drawable.ic_hintsearch_16),
-                null,
-                null,
-                null
-            )
+            } else {
+                clearEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                    ContextCompat.getDrawable(this@SearchActivity, R.drawable.ic_hintsearch_16),
+                    null,
+                    null,
+                    null
+                )
+            }
+
         }
-
-    }
 
 
     @SuppressLint("ClickableViewAccessibility")
     private fun clearTextFromEditText() {
-        val clearEditText =
-            findViewById<androidx.appcompat.widget.AppCompatEditText>(R.id.search_stroke)
+        var clearEditText =
+            findViewById<AppCompatEditText>(R.id.search_stroke)
         val inputMethodManager =
             getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         clearEditText.setOnTouchListener { view, event ->
@@ -219,12 +225,11 @@ class SearchActivity : AppCompatActivity() {
                             0
                         ) // Прячем клаву
                         // чистим эдит текст
-                        recyclerView.visibility =
-                            View.INVISIBLE  // убрали список треков при очистке эдиттекста
-                        msgTopTxt.visibility = View.GONE  //Убрали сообщение топ
-                        msgBotTxt.visibility = View.GONE // убрали сообщение бот
-                        phForNothingToShow.visibility = View.GONE // убрали плейсхолдер
-                        buttonNoInternet.visibility = View.GONE // убрали кнопку
+                        recyclerView.makeInvisible()  // убрали список треков при очистке эдиттекста
+                        msgTopTxt.makeGone()  //Убрали сообщение топ
+                        msgBotTxt.makeGone() // убрали сообщение бот
+                        phForNothingToShow.makeGone() // убрали плейсхолдер
+                        buttonNoInternet.makeGone() // убрали кнопку
 
                         return@setOnTouchListener true
                     }
@@ -250,18 +255,18 @@ class SearchActivity : AppCompatActivity() {
                                 R.drawable.ph_nothing_to_show_120
                             )
                             phForNothingToShow.setImageDrawable(phNts)
-                            phForNothingToShow.visibility = View.VISIBLE
-                            msgTopTxt.visibility = View.VISIBLE
+                            phForNothingToShow.makeVisible()
+                            msgTopTxt.makeVisible()
                             msgTopTxt.text = getString(R.string.msg_nothing_to_show)
                         } else {
-                            phForNothingToShow.visibility = View.GONE
-                            msgBotTxt.visibility = View.GONE
-                            msgTopTxt.visibility = View.GONE
-                            buttonNoInternet.visibility = View.GONE
+                            phForNothingToShow.makeGone()
+                            msgBotTxt.makeGone()
+                            msgTopTxt.makeGone()
+                            buttonNoInternet.makeGone()
                             val recyclerView = findViewById<RecyclerView>(R.id.track_list)
                             recyclerView.layoutManager = LinearLayoutManager(this@SearchActivity)
                             recyclerView.adapter = TrackAdapter(tracks)
-                            recyclerView.visibility = View.VISIBLE
+                            recyclerView.makeVisible()
 
                         }
                     } else {
@@ -270,12 +275,12 @@ class SearchActivity : AppCompatActivity() {
                             R.drawable.ph_no_internet_120
                         )
                         phForNothingToShow.setImageDrawable(phNts) // To do
-                        phForNothingToShow.visibility = View.VISIBLE
-                        msgTopTxt.visibility = View.VISIBLE
+                        phForNothingToShow.makeVisible()
+                        msgTopTxt.makeVisible()
                         msgTopTxt.text = getString(R.string.msg_no_internet_top)
-                        msgBotTxt.visibility = View.VISIBLE
+                        msgBotTxt.makeVisible()
                         msgBotTxt.text = getString(R.string.msg_no_internet_bottom)
-                        buttonNoInternet.visibility = View.VISIBLE
+                        buttonNoInternet.makeVisible()
 
                     }
                 }
@@ -286,19 +291,30 @@ class SearchActivity : AppCompatActivity() {
                         R.drawable.ph_no_internet_120
                     )
                     phForNothingToShow.setImageDrawable(phNts) // To do
-                    phForNothingToShow.visibility = View.VISIBLE
-                    msgTopTxt.visibility = View.VISIBLE
+                    phForNothingToShow.makeVisible()
+                    msgTopTxt.makeVisible()
                     msgTopTxt.text = getString(R.string.msg_no_internet_top)
-                    msgBotTxt.visibility = View.VISIBLE
+                    msgBotTxt.makeVisible()
                     msgBotTxt.text = getString(R.string.msg_no_internet_bottom)
-                    buttonNoInternet.visibility = View.VISIBLE
+                    buttonNoInternet.makeVisible()
 
                 }
+
 
 
             }
 
         }
+    }
+    private fun View.makeGone(){
+        this.visibility = View.GONE // функция для вью гон
+    }
+
+    private fun View.makeVisible(){
+        this.visibility = View.VISIBLE // функция для вью визибл
+    }
+    private fun View.makeInvisible(){
+        this.visibility = View.INVISIBLE // функция для вью инвизибл
     }
 
 }
