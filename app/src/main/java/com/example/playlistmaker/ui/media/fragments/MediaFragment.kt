9798d990a69b58179c36d5179e5edaf8284a1fca
Evgen1994-McDgit config --global user.email mrmarwell@gmail.comgit config --listgit config --global user.name Evgen1994-McD <mrmarwell@gmail.com>
2025-07-01 13:41:47 +0500

    Настроил сохранение вкладок Tablayout.
    Не нужно было инициализировать через Lazy, сделал всё в onViewCreated и заработало

--- app/src/main/java/com/example/playlistmaker/ui/media/fragments/MediaFragment.kt
+++ app/src/main/java/com/example/playlistmaker/ui/media/fragments/MediaFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentMediaBinding
 import com.example.playlistmaker.ui.media.activity.ActivityMediaCatalogue.Companion.savedPage
@@ -17,11 +18,11 @@ import org.koin.androidx.viewmodel.ext.android.activityViewModel
 
 class MediaFragment : Fragment() {
     private lateinit var binding: FragmentMediaBinding
-    private val pager by lazy { binding.viewpager }
-    private val tabs by lazy { binding.tabLayout }
-    private val vpAdapter by lazy { VpAdapter(this) }
     private val mediaFragmentViewModel: MediaFragmentViewModel by activityViewModel()
     private var currentPagePosition = 0
+    private lateinit var pager: ViewPager2
+    private lateinit var tabs: TabLayout
+    private lateinit var vpAdapter: VpAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,17 +30,29 @@ class MediaFragment : Fragment() {
     ): View {
         binding = FragmentMediaBinding.inflate(inflater, container, false)
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+val tabs = binding.tabLayout
+        val pager = binding.viewpager
+        val vpAdapter = VpAdapter(this)
+
+        if (pager.adapter== null) {
+            pager.adapter = vpAdapter
+        } else {
+            pager.adapter!!.notifyDataSetChanged()
+        }
+        pager.currentItem = currentPagePosition
 
         // Получаем сохранённую позицию вкладки
         savedInstanceState?.let {
             currentPagePosition = it.getInt(savedPage, 0)
         }
 
-        setupTabsAndPager()
+    setupTabsAndPager(pager, tabs)
+
 
         // Следим за сменой вкладок
         tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
@@ -53,15 +66,30 @@ class MediaFragment : Fragment() {
         })
     }
 
+    override fun onResume() {
+        super.onResume()
+        val tabs = binding.tabLayout
+        val pager = binding.viewpager
+        val vpAdapter = VpAdapter(this)
+        if (pager.adapter== null) {
+            pager.adapter = vpAdapter
+        } else {
+            pager.adapter!!.notifyDataSetChanged()
+        }
+        setupTabsAndPager(pager, tabs)
+
+    }
+
+
+
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         // Сохраняем текущее положение вкладки
         outState.putInt(savedPage, currentPagePosition)
     }
 
-    private fun setupTabsAndPager() {
-        pager.adapter = vpAdapter
-        pager.currentItem = currentPagePosition
+    private fun setupTabsAndPager(pager: ViewPager2, tabs: TabLayout) {
+
 
 
         TabLayoutMediator(tabs, pager) { tab, position ->
