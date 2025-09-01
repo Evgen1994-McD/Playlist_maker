568c649f9d66b9dc931d31c022ecaa0f1b5a1991
Evgen1994-McDgit config --global user.email mrmarwell@gmail.comgit config --listgit config --global user.name Evgen1994-McD <mrmarwell@gmail.com>
2025-09-01 16:17:51 +0500

    Добавление/удаление из плейлиста

--- app/src/main/java/com/example/playlistmaker/ui/media/viewmodel/PlaylistTracksViewModel.kt
+++ app/src/main/java/com/example/playlistmaker/ui/media/viewmodel/PlaylistTracksViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.room.util.copy
+import com.example.playlistmaker.domain.models.PlayList
 import com.example.playlistmaker.domain.models.Track
 import com.example.playlistmaker.domain.playlists.PlaylistInteractor
 import com.example.playlistmaker.utils.TimeUtils
@@ -14,7 +15,7 @@ import kotlinx.coroutines.launch
 
 class PlaylistTracksViewModel(private val playlistInteractor: PlaylistInteractor,
     private val playListId: Int) : ViewModel() {
-
+private lateinit var currentPlayList:PlayList
     private val playListTracksLiveData1 = MutableLiveData<PlayListTracksScreenState>()
     val getPlaylistTracks1LiveData : LiveData<PlayListTracksScreenState> get() = playListTracksLiveData1
 init {
@@ -35,7 +36,12 @@ init {
     }
 
     fun deleteTrackOfPlaylistById(trackId: String)=viewModelScope.launch{
+        val currentTracks = currentPlayList.tracksId.split(",")
+        val filteredElements = currentTracks.filterNot { it.trim()==trackId.trim() }
+        val filteredTracks = filteredElements.joinToString(separator = ",")
         playlistInteractor.deleteTrackOfPlaylistById(trackId)
+        playlistInteractor.insertPlayList(currentPlayList.copy(size = currentPlayList.size-1, tracksId = filteredTracks))
+        getPlayListState()
 
     }
 
@@ -44,7 +50,7 @@ init {
         try {
 
 
-            val currentPlayList = playlistInteractor.getPlaylistById(playListId)
+            currentPlayList = playlistInteractor.getPlaylistById(playListId)
             val trackList = playlistInteractor.getTrackOfPlaylistById(currentPlayList.tracksId)
             val timeForScreenState = tracksTimeInMinutes(trackList)
             playListTracksLiveData1.postValue(
