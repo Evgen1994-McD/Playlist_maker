6f1fdaf1e59b79b539d60f7bc94a2a2be33e4084
Evgen1994-McDgit config --global user.email mrmarwell@gmail.comgit config --listgit config --global user.name Evgen1994-McD <mrmarwell@gmail.com>
2025-09-03 14:04:23 +0500

    ап

--- app/src/main/java/com/example/playlistmaker/ui/media/fragments/ReplacePlayListFragment.kt
+++ app/src/main/java/com/example/playlistmaker/ui/media/fragments/ReplacePlayListFragment.kt
@@ -16,15 +16,15 @@ import org.koin.core.parameter.parametersOf
 
 class ReplacePlayListFragment(
 
-):AddPlayListFragment() {
-    override val viewModel: ReplacePlaylistViewModel by viewModel(){
+) : AddPlayListFragment() {
+    override val viewModel: ReplacePlaylistViewModel by viewModel() {
         parametersOf(
             getPlaylistIdFromArguments(REPLACE_LISTID)
         )
     }
 
 
-    companion object{
+    companion object {
 
         const val REPLACE_LISTID = "listId"
 
@@ -33,52 +33,45 @@ class ReplacePlayListFragment(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-observePlaylistData()
+        observePlaylistData()
         viewModel.getReplacePlaylist()
 
 
     }
 
 
-    private fun observePlaylistData()=viewModel.getLiveData.observe(viewLifecycleOwner){ playlist->
-ur1 = playlist.image?.toUri()
+    private fun observePlaylistData() =
+        viewModel.getLiveData.observe(viewLifecycleOwner) { playlist ->
+            ur1 = playlist.image?.toUri()
 
 
-        binding.edPlaylistName.setText(playlist.name)
-        binding.edAboutPlaylist.setText(playlist.about)
-        binding.imMines.setImageURI(playlist.image?.toUri())
-        if (playlist.image!="null"){
-            binding.ph.isVisible = false
-        } else binding.ph.isVisible = true
+            binding.edPlaylistName.setText(playlist.name)
+            binding.edAboutPlaylist.setText(playlist.about)
+            binding.imMines.setImageURI(playlist.image?.toUri())
+            if (playlist.image != "null") {
+                binding.ph.isVisible = false
+            } else binding.ph.isVisible = true
 
 
-        binding.toolbar.setNavigationOnClickListener {
-            findNavController().popBackStack()
+            binding.toolbar.setNavigationOnClickListener {
+                findNavController().popBackStack()
+            }
+
+            binding.toolbar.setTitle(R.string.replace_playlist_title)
+            binding.btSave.setText(R.string.replace_playlist_button)
+
+            binding.btSave.setOnClickListener {
+                val playList = playlist.copy(
+                    listId = playlist.listId,
+                    name = title.toString(),
+                    about = text.toString(),
+                    image = ur1.toString()
+                )
+
+
+                viewModel.savePlayList(playList)
+                findNavController().popBackStack()
+            }
+
         }
-
-        binding.toolbar.setTitle(R.string.replace_playlist_title)
-        binding.btSave.setText(R.string.replace_playlist_button)
-
-        binding.btSave.setOnClickListener {
-            val playList = playlist.copy(
-                listId = playlist.listId,
-                name = title.toString(),
-                about = text.toString(),
-                image = ur1.toString()
-            )
-
-
-            viewModel.savePlayList(playList)
-findNavController().popBackStack()
-        }
-
-
-
-
-    }
-
-
-
-
-
 }
